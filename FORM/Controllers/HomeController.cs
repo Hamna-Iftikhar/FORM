1399a7984dea3dfb4a6465af842566e8e7922458
DELL <DELL@DESKTOP-CHF0QQU>
2025-12-02 11:14:13 +0500

    Add project files.

---
+++ FORM/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+using FORM.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Xml.Linq;
+
+
+namespace FORM.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public  String Index(Employee e)
+        {
+            return "Name:" + e.Name + "Gender:" + e.Gender + "Age:" + e.Age + "Designation" + e.Designation + "Salary:" + e.Salary + "Married" + e.Married + "Description" + e.Description;
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ FORM/Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿namespace FORM.Models
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+
+        //Specify Add dropdownlist
+        public Gender Gender { get; set; }
+        public int Age { get; set; }
+
+        public string Designation { get; set; }
+
+        public int Salary { get; set; }
+        //Add Radio button
+        public string Married { get; set; }
+        //Add textbox
+        public string Description { get; set; }
+    }
+    //Add dropdownlist
+    public enum Gender
+    {
+        Male,Female
+    }
+}
+
